117e42911de222860f5d5be9ce0d7df80a042d63
tetra <me@tetra.cool>
2022-08-16 16:06:04 -0600

    remove back buton

--- src/components/includes/header/header.svelte
+++ src/components/includes/header/header.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-	import { afterNavigate, goto } from "$app/navigation";
 	import "./style.scss";
 
 	let scrollY: number;
-	let isRoot = true;
-
-	afterNavigate((navigation: { from: URL | null; to: URL }) => {
-		if (!navigation.to) return;
-		isRoot = navigation.to.pathname === "/";
-	});
-
-	const back = () => {
-		window.history.back();
-	};
 </script>
 
 <svelte:window bind:scrollY />
@@ -20,9 +9,6 @@
 <header class={scrollY > 0 ? "scrolled" : ""}>
 	<span id="gradient" />
 	<div class="row">
-		{#if !isRoot}
-			<a href="/" on:click|preventDefault={back}><span class="material-icons md-48">arrow_back</span></a>
-		{/if}
 		<a id="homelink" class="row" href="/">
 			<span id="logo" />
 			<span id="hometext">AnthroSpace</span>
