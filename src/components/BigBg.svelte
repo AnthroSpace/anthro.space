7784c5fe892aef3baf12dd08791793cb88a18a95
tetra <me@tetra.cool>
2023-03-14 12:07:36 -0600

    document check

--- src/components/BigBg.svelte
+++ src/components/BigBg.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { dev } from "$app/environment";
   import * as PIXI from "pixi.js";
   import "$lib/pixi-setsize"; // neat :)
   import { KawaseBlurFilter, AdjustmentFilter } from "pixi-filters";
@@ -19,12 +20,19 @@
       ]
     : [];
 
-  onMount(async () => {
+  onMount(() => {
+    if (!document) return; // dude i have no idea what's going on
     const app = new PIXI.Application({
       antialias: true,
       resizeTo: window,
       view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
     });
+
+    if (dev) globalThis.__PIXI_APP__ = app;
+
+    let elapsed = 0.0;
+
+    app.ticker.add((delta) => (elapsed += delta));
 
     let bg = PIXI.Sprite.from(
       isVideo ? (document.getElementById("pixi-video") as HTMLVideoElement) : src
@@ -32,44 +40,9 @@
 
     bg.filters = baseFilters.concat(filters);
 
-    // create a particle container to hold the particles
-    const particles = new PIXI.ParticleContainer(10000, {
-      scale: true,
-      position: true,
-      rotation: true,
-      uvs: true,
-      alpha: true,
-    });
-
     app.stage.addChild(bg);
 
-    // add particles
-    for (let i = 0; i < 10; i++) {
-      const sprite = PIXI.Sprite.from(src);
-      sprite.x = Math.random() * app.renderer.width;
-      sprite.y = Math.random() * app.renderer.height;
-      sprite.width = 10;
-      sprite.height = 10;
-      sprite.tint = 0x000000;
-      sprite.alpha = 0.5;
-      particles.addChild(sprite);
-    }
-
-    let elapsed = 0.0;
-
-    app.ticker.add((delta) => {
-      elapsed += delta;
-      // animate particles
-      for (let i = 0; i < particles.children.length; i++) {
-        const sprite = particles.children[i] as PIXI.Sprite;
-        sprite.x += Math.sin(elapsed * 0.0001 + i) * 10;
-        sprite.y += Math.cos(elapsed * 0.0001 + i) * 10;
-        if (sprite.x < 0) sprite.x += app.renderer.width;
-        else if (sprite.x > app.renderer.width) sprite.x -= app.renderer.width;
-        if (sprite.y < 0) sprite.y += app.renderer.height;
-        else if (sprite.y > app.renderer.height) sprite.y -= app.renderer.height;
-      }
-
+    app.ticker.add(() => {
       // bg.filters.forEach((f) => (f.time = elapsed));
 
       bg.setSize(app.renderer.width, app.renderer.height, "cover");
@@ -77,7 +50,7 @@
   });
 </script>
 
-<div id="background">
+<div>
   {#if isVideo}
     <video {src} muted autoplay loop playsinline id="pixi-video" />
   {/if}
@@ -103,16 +76,5 @@
     & > video {
       display: none;
     }
-
-    // & > video,
-    // & > img {
-    //   position: absolute;
-    //   top: 50%;
-    //   left: 50%;
-    //   transform: translate(-50%, -50%);
-    //   width: 100%;
-    //   height: 100%;
-    //   object-fit: cover;
-    // }
   }
 </style>
